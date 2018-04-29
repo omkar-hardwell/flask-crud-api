e068206963dcc9c1bd29ec5c4068f9f50c35c18d
omkar-hardwell <omkarkshirsagar862@gmail.com>
2018-04-29 17:31:12 +0530

    GET endpoint to check health of the application.

--- configs.py
+++ configs.py
@@ -2,6 +2,7 @@
 
 # Api information
 API_NAME = 'flask-crud-api'
+BASE_PATH = '/v1'
 
 # Swagger specification file path
 SWAGGER_SPEC_PATH = 'spec/swagger.yaml'
--- src/api.py
+++ src/api.py
@@ -1,15 +1,25 @@
-"""Application"""
+"""Application setup
+   1) Swagger UI
+   2) API routes
+"""
 import configs
 
-from flask import Flask
+from flask import Flask, jsonify
 from flasgger import Swagger
 
 
 # Application instance
 app = Flask(configs.API_NAME)
 
+# Swagger UI integration
 app.config['SWAGGER'] = {
    'title': 'Flask Api - CRUD',
    'uiversion': 2
 }
 Swagger(app, template_file=configs.SWAGGER_SPEC_PATH)
+
+
+@app.route(configs.BASE_PATH + '/hello', methods=['GET'])
+def health():
+    """Check the health of the application."""
+    return jsonify({'status': 'ok'})
