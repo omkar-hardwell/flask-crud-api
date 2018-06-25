3fd9c11b4445c71fd454feab49554b407ae4ec7d
omkar-hardwell <omkarkshirsagar862@gmail.com>
2018-06-25 13:17:30 +0530

    Updated HTTP error messages.

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,8 @@ GENDER = ['male', 'female']
 
 # HTTP response error messages
 ERROR_MESSAGE_NOT_FOUND = 'Requested {title} {id} not found.'
-ERROR_MESSAGE_BAD_REQUEST = '{id} must be integer and greater than zero'
+ERROR_MESSAGE_BAD_REQUEST = \
+    '{title} - {id} must be integer and greater than zero'
 ERROR_MESSAGE_INTERNAL_ERROR = 'Could not connect to MySQL.'
 
 # HTTP response error codes
