edcd5be3f8b0bfe79f049fa71d105ad3c858a50b
omkar-hardwell <omkarkshirsagar862@gmail.com>
2018-07-08 13:12:57 +0530

    Database configuration changed

--- configs.py
+++ configs.py
@@ -14,7 +14,24 @@ Testing: test
 '''
 ENVIRONMENT = 'dev'
 
+# Database credentials
+DB_CREDENTIALS = {
+    'username': 'username',
+    'password': 'password',
+    'host': 'host',
+    'port': 3306,
+    'database': 'database'
+}
+
 # Database connection uri
 DATABASE_URI = 'sqlite://'
 if ENVIRONMENT == 'dev':
-    DATABASE_URI = 'mysql+pymysql://username:password@localhost/db_name'
+    DATABASE_URI = \
+        'mysql+pymysql://{username}:{password}@{host}:{port}/{database}'.\
+        format(
+            username=DB_CREDENTIALS.get('username'),
+            password=DB_CREDENTIALS.get('password'),
+            host=DB_CREDENTIALS.get('host'),
+            port=DB_CREDENTIALS.get('port'),
+            database=DB_CREDENTIALS.get('database')
+        )
